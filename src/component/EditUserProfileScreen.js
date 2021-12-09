b1d6aba65dd0fe56648544265d3842cca4f869e3
Lexo783 <florentdebuchy@orange.fr>
2021-12-09 17:18:25 +0100

    fix eslint

--- src/component/EditUserProfileScreen.js
+++ src/component/EditUserProfileScreen.js
@@ -1,84 +1,87 @@
-import * as React from "react";
-import { 
-    SafeAreaView,
-    Image,
-    Text,
-    View,
-    TouchableOpacity,
-    StyleSheet,
-} from "react-native";
-  
-const ProfileScreen = (props) => {
-    const {navigation} = props;
+import * as React from 'react';
+import {
+  SafeAreaView,
+  Image,
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
-    return (
-        <SafeAreaView style={styles.profileContainer}>
-            <View style={styles.userData}>
-                <View style={styles.user}>
-                    <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
-                        <Image style={styles.userAvatar}
-                            source={{uri: 'https://icon-library.com/images/icon-spiderman/icon-spiderman-16.jpg',}}
-                        />
-                    </TouchableOpacity>
-                    <View style={styles.userInfos}>
-                        <Text style={styles.username}>John Doe</Text>
-                        <Text style={styles.pseudo}>mail@mail.com</Text>
-                    </View>
-                </View>
-            </View>
-            <TouchableOpacity style={styles.editBtn} onPress={() => {}}>
-                <Text>Modifier mon profil</Text>
-            </TouchableOpacity>
-        </SafeAreaView>
-    )
-}
+const ProfileScreen = props => {
+  const {navigation} = props;
+
+  return (
+    <SafeAreaView style={styles.profileContainer}>
+      <View style={styles.userData}>
+        <View style={styles.user}>
+          <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+            <Image
+              style={styles.userAvatar}
+              source={{
+                uri: 'https://icon-library.com/images/icon-spiderman/icon-spiderman-16.jpg',
+              }}
+            />
+          </TouchableOpacity>
+          <View style={styles.userInfos}>
+            <Text style={styles.username}>John Doe</Text>
+            <Text style={styles.pseudo}>mail@mail.com</Text>
+          </View>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.editBtn} onPress={() => {}}>
+        <Text>Modifier mon profil</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-    profileContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    userData: {
-        paddingHorizontal: 30,
-        marginBottom: 25,
-    },
-    user: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    userAvatar: {
-        width: 200,
-        height: 200,
-        borderColor: '#000',
-        borderWidth: 1,
-        borderRadius: 150/2,
-    },
-    username: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 15,
-        marginBottom: 5,
-        textAlign: 'center',
-    },
-    pseudo: {
-        fontSize: 14,
-        lineHeight: 14,
-        fontWeight: '500',
-        textAlign: 'center',
-    },
-    row: {
-        flexDirection: 'row',
-        marginBottom: 10,
-    },
-    rowText: {
-        color: '#777777',
-    },
-    editBtn: {
-        backgroundColor: '#23A25C',
-        borderRadius: 15,
-        padding: 12,
-    },
-})
+  profileContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  userData: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  user: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  userAvatar: {
+    width: 200,
+    height: 200,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 150 / 2,
+  },
+  username: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  pseudo: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  rowText: {
+    color: '#777777',
+  },
+  editBtn: {
+    backgroundColor: '#23A25C',
+    borderRadius: 15,
+    padding: 12,
+  },
+});
